5cffb26225d8417c36ff25ab316285380db11800
OspreyFly <noahbengoff@gmail.com>
2024-08-05 11:52:37 -0500

    exercise complete

--- bubble.js
+++ bubble.js
@@ -1,3 +1,16 @@
-function bubbleSort() {}
+function bubbleSort(array) {
+    let len = array.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Swap elements
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,37 @@
-function merge() {}
+function merge(arr1, arr2) {
+    const result = [];
+    while (arr1.length || arr2.length) {
+        if (!arr1.length) {
+            result.push(...arr2);
+            break;
+        } else if (!arr2.length) {
+            result.push(...arr1);
+            break;
+        } else if (arr1[0] <= arr2[0]) {
+            result.push(arr1[0]);
+            arr1 = arr1.slice(1); 
+        } else {
+            result.push(arr2[0]);
+            arr2 = arr2.slice(1); 
+        }
+    }
+    return result;
+}
 
-function mergeSort() {}
+
+
+
+function mergeSort(array) {
+    if (array.length < 2) {
+        return array;
+    }
+
+    const mid = Math.floor(array.length / 2);
+    const left = array.slice(0, mid);
+    const right = array.slice(mid);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
 
 module.exports = { merge, mergeSort};
